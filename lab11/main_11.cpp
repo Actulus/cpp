a68300fc1f65e674e0407f56e2d922982ca526cb
Actulus <schiflerpatricia@gmail.com>
2022-12-08 15:22:23 +0200

    reading from file using map and regex, where specific words are keys and the lines in which contain these words are the values

--- lab11/main_11.cpp
+++ lab11/main_11.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include <cstring>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <regex>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, Lab_11" << std::endl;
-    return 0;
+	ifstream fin;
+	fin.open("input.txt");
+	if (fin.fail()) {
+		throw runtime_error("Cannot open file");
+	}
+
+	map<string, vector<int>> indexes;
+	string line;
+	vector<int> temp;
+	int counter = 1;
+
+	while (getline(fin, line) && !line.empty()) {
+		std::transform(line.begin(), line.end(), line.begin(), ::toupper);
+		indexes.insert(make_pair(line, temp));
+	}
+
+	while (fin.peek() != EOF) { //
+		getline(fin, line);
+		std::transform(line.begin(), line.end(), line.begin(), ::toupper);
+		istringstream iss(line);
+		//cout << line << endl;
+		for (auto &i: indexes) {
+			string temp = "\\b" + i.first + "\\b";
+			regex r(temp);
+			smatch match;
+
+			//if (line.find(i.first) != string::npos) {
+			if (regex_search(line, match, r)) {
+				//cout << i.first << " " << counter << endl;
+				i.second.push_back(counter);
+			}
+		}
+		counter++;
+	}
+
+
+	for (const auto &index: indexes) {
+		cout << index.first << " ";
+		if (index.second.size() == 1) {
+			cout << index.second[0] << " ";
+		} else {
+			for (auto i = 0; i < index.second.size(); ++i) {
+				if (index.second[i] == index.second[i + 1] - 1) {
+					cout << index.second[i] << "-";
+				} else if (i == index.second.size() - 1) {
+					cout << index.second[i] << " ";
+				} else {
+					cout << index.second[i] << ", ";
+				}
+			}
+		}
+		cout << endl;
+	}
+
+	fin.close();
+	return 0;
 }
